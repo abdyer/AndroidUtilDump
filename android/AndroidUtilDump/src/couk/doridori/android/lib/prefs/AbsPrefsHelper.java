badd3ef8be91f3c3903f6a12a33492a6e5586713
Dorian Cussen <github@doridori.co.uk>
2012-11-01 16:22:51 +0000

    prefs helper

---
+++ android/AndroidUtilDump/src/couk/doridori/android/lib/prefs/AbsPrefsHelper.java
@@ -0,0 +1,48 @@
+package couk.doridori.android.lib.prefs;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.AsyncTask;
+import android.os.Build;
+
+/**
+ * In your project you can extends this class and add your project specific KEYS
+ *
+ * User: doriancussen
+ * Date: 01/11/2012
+ */
+public abstract class AbsPrefsHelper {
+
+    private static SharedPreferences sSharedPreferences;
+
+    public static synchronized SharedPreferences getDefaultSharedPreferences(Context context){
+        if(sSharedPreferences == null)
+            sSharedPreferences = context.getSharedPreferences("defaultPrefs", Context.MODE_PRIVATE);
+
+        return sSharedPreferences;
+    }
+
+    public static synchronized String getString(Context context, String key, String defValue){
+        return getDefaultSharedPreferences(context).getString(key, defValue);
+    }
+
+    public static synchronized SharedPreferences.Editor getEditor(Context ctx){
+        return getDefaultSharedPreferences(ctx).edit();
+    }
+
+    public static synchronized void asyncCommit(final SharedPreferences.Editor editor){
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.GINGERBREAD){
+            editor.apply();
+        }else{
+            new AsyncTask<Void, Void, Void>(){
+                @Override
+                protected Void doInBackground(Void... params) {
+                    editor.commit();
+                    return null;
+                }
+            }.execute();
+        }
+    }
+
+
+}
