255035f78b25a1865e0bd0c72448661d13985035
Dorian Cussen <github@doridori.co.uk>
2012-11-02 11:08:25 +0000

    doc change

--- android/AndroidUtilDump/src/couk/doridori/android/lib/view/SafeButton.java
+++ android/AndroidUtilDump/src/couk/doridori/android/lib/view/SafeButton.java
@@ -7,7 +7,11 @@ import android.view.animation.AnimationUtils;
 import android.widget.Button;
 
 /**
- * Class to work around the annoying queuing of multiple taps for Buttons. Could have a time attribute
+ * Class to work around the annoying queuing of multiple taps for Buttons. Could have a time attribute.
+ *
+ * This is useful as sometimes (on some platform / device combos) you can quickly tap on a button and end up
+ * starting an activity more than one. The reuests seem to be queued before the action has taken place so
+ * solutions like disabling the buttons or setting singleTop do not seem to work :(
  *
  * @author dori
  */
