1732714f4f3c833305fed055980745f61e9a0e40
Dorian Cussen <github@doridori.co.uk>
2012-11-21 15:13:04 +0000

    session support

---
+++ android/AndroidUtilDump/src/couk/doridori/android/lib/model/ISession.java
@@ -0,0 +1,9 @@
+package couk.doridori.android.lib.model;
+
+/**
+ * @author Dorian Cussen
+ *         Date: 21/11/2012
+ */
+public interface ISession {
+    public boolean isSessionValid();
+}
---
+++ android/AndroidUtilDump/src/couk/doridori/android/lib/ui/InSessionActivity.java
@@ -0,0 +1,63 @@
+package couk.doridori.android.lib.ui;
+
+import android.content.Intent;
+import android.os.Bundle;
+import com.actionbarsherlock.app.SherlockFragmentActivity;
+import couk.doridori.android.lib.model.ISession;
+import couk.doridori.android.lib.util.XLog;
+
+/**
+ * This activity provides alt lifecycle methods so if the session call fails your activity will not have its own
+ * init  functionality called in the traditional lifecycle methods
+ *
+ * User: doriancussen
+ * Date: 09/11/2012
+ */
+public abstract class InSessionActivity extends SherlockFragmentActivity {
+
+    private boolean mIsSessionValid;
+
+    @Override
+    public final void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if(null == getSession()){
+            //session invalid - jump back to session start activity
+            XLog.w("Session INVALID - jumping back to start activity");
+            mIsSessionValid = false;
+            Intent intent = new Intent(this, getSessionStartActivityClass());
+            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity(intent);
+            finish();
+        }else{
+            mIsSessionValid = true;
+            onCreatePostSessionCheck(savedInstanceState);
+        }
+    }
+
+    @Override
+    protected final void onStart() {
+        super.onStart();
+        if(mIsSessionValid){
+            onStartPostSessionCheck();
+        }
+    }
+
+    @Override
+    protected final void onResume() {
+        super.onResume();
+        if(mIsSessionValid){
+            onResumePostSessionCheck();
+        }
+    }
+
+    protected abstract void onCreatePostSessionCheck(Bundle savedInstanceState);
+    protected void onStartPostSessionCheck(){};
+    protected void onResumePostSessionCheck(){};
+
+    /**
+     * @return return the class of the activity that initiates the session here
+     */
+    protected abstract Class getSessionStartActivityClass();
+    protected abstract ISession getSession();
+}
