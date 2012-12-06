a456be490b3a734ebe78aa0c9c0a1d2cbc41fd98
Dorian Cussen <github@doridori.co.uk>
2012-12-06 14:57:34 +0000

    XLog uses class as tag

--- android/AndroidUtilDump/src/couk/doridori/android/lib/util/XLog.java
+++ android/AndroidUtilDump/src/couk/doridori/android/lib/util/XLog.java
@@ -2,7 +2,8 @@ package couk.doridori.android.lib.util;
 
 import android.util.Log;
 
-public class XLog {
+public class XLog
+{
 
     private static final int NONE = 0;
     private static final int ERRORS_ONLY = 1;
@@ -13,105 +14,139 @@ public class XLog {
 
     private static final int LOGGING_LEVEL_FILTER = ALL;
 
-    private static final String TAG_NO_FILTER = "XLog";
-
-    static {
-        if(LOGGING_LEVEL_FILTER >= ALL)
-            Log.v(TAG_NO_FILTER, "ILog class reloaded");
+    static
+    {
+        if (LOGGING_LEVEL_FILTER >= ALL)
+            Log.v(XLog.class.getName(), "ILog class reloaded");
     }
 
-    public static void e(String text, Exception e) {
-
+    public static void e(String text, Exception e)
+    {
         e(text + " : " + e.getClass().getName() + ": " + e.getMessage());
     }
 
-    public static void e(String text) {
-
-        if (LOGGING_LEVEL_FILTER >= ERRORS_ONLY) {
-            Log.e(TAG_NO_FILTER, getTrace() + text);
+    public static void e(String text)
+    {
+        if (LOGGING_LEVEL_FILTER >= ERRORS_ONLY)
+        {
+            Trace trace = getTrace();
+            Log.e(trace.clazz, trace.trace + text);
         }
     }
 
-    public static void w(String text) {
-
-        if (LOGGING_LEVEL_FILTER >= ERRORS_WARNINGS) {
-            Log.w(TAG_NO_FILTER, getTrace() + text);
+    public static void w(String text)
+    {
+        if (LOGGING_LEVEL_FILTER >= ERRORS_WARNINGS)
+        {
+            Trace trace = getTrace();
+            Log.w(trace.clazz, getTrace().trace + text);
         }
     }
 
-    public static void i(String text) {
-
-        if (LOGGING_LEVEL_FILTER >= ERRORS_WARNINGS_INFO) {
-            Log.i(TAG_NO_FILTER, getTrace() + text);
+    public static void i(String text)
+    {
+        if (LOGGING_LEVEL_FILTER >= ERRORS_WARNINGS_INFO)
+        {
+            Trace trace = getTrace();
+            Log.i(trace.clazz, getTrace().trace + text);
         }
     }
 
-    public static void d() {
-
-        if (LOGGING_LEVEL_FILTER >= ERRORS_WARNINGS_INFO_DEBUG) {
-            Log.d(TAG_NO_FILTER, getTrace() + "<method>");
+    public static void d()
+    {
+        if (LOGGING_LEVEL_FILTER >= ERRORS_WARNINGS_INFO_DEBUG)
+        {
+            Trace trace = getTrace();
+            Log.d(trace.clazz, getTrace().trace + "<method>");
         }
     }
 
-    public static void d(Object obj){
-        if (LOGGING_LEVEL_FILTER >= ERRORS_WARNINGS_INFO_DEBUG) {
-            Log.d(obj.toString(), getTrace());
+    public static void d(Object obj)
+    {
+        if (LOGGING_LEVEL_FILTER >= ERRORS_WARNINGS_INFO_DEBUG)
+        {
+            Trace trace = getTrace();
+            Log.d(obj.toString(), getTrace().trace);
         }
     }
 
-    public static void d(String text) {
-
-        if (LOGGING_LEVEL_FILTER >= ERRORS_WARNINGS_INFO_DEBUG) {
-            Log.d(TAG_NO_FILTER, getTrace() + text);
+    public static void d(String text)
+    {
+        if (LOGGING_LEVEL_FILTER >= ERRORS_WARNINGS_INFO_DEBUG)
+        {
+            Trace trace = getTrace();
+            Log.d(trace.clazz, getTrace().trace + text);
         }
     }
 
-    public static void d(String tag, String text) {
-
-        if (LOGGING_LEVEL_FILTER >= ERRORS_WARNINGS_INFO_DEBUG) {
-            Log.d(tag, getTrace() + text);
+    public static void dn(String text)
+    {
+        if (LOGGING_LEVEL_FILTER >= ERRORS_WARNINGS_INFO_DEBUG)
+        {
+            Trace trace = getTrace();
+            Log.d(trace.clazz, "");//blank line
+            Log.d(trace.clazz, getTrace().trace + text);
         }
     }
 
-    public static void d(Object obj, String text) {
-
-        if (LOGGING_LEVEL_FILTER >= ERRORS_WARNINGS_INFO_DEBUG) {
-            Log.d(obj.getClass().getName(), getTrace() + text);
+    public static void d(String tag, String text)
+    {
+        if (LOGGING_LEVEL_FILTER >= ERRORS_WARNINGS_INFO_DEBUG)
+        {
+            Trace trace = getTrace();
+            Log.d(tag, getTrace().trace + text);
         }
     }
 
-    public static void v() {
-
-        if (LOGGING_LEVEL_FILTER >= ALL) {
-            Log.v(TAG_NO_FILTER, getTrace() + "<method>");
+    public static void d(Object obj, String text)
+    {
+        if (LOGGING_LEVEL_FILTER >= ERRORS_WARNINGS_INFO_DEBUG)
+        {
+            Trace trace = getTrace();
+            Log.d(obj.getClass().getName(), getTrace().trace + text);
         }
     }
 
-    public static void v(String text) {
+    public static void v()
+    {
+        if (LOGGING_LEVEL_FILTER >= ALL)
+        {
+            Trace trace = getTrace();
+            Log.v(trace.clazz, getTrace().trace + "<method>");
+        }
+    }
 
-        if (LOGGING_LEVEL_FILTER >= ALL) {
-            Log.v(TAG_NO_FILTER, getTrace() + text);
+    public static void v(String text)
+    {
+        if (LOGGING_LEVEL_FILTER >= ALL)
+        {
+            Trace trace = getTrace();
+            Log.v(trace.clazz, getTrace().trace + text);
         }
     }
 
     /**
      * print the stack of execution path to log output - sometimes .printStackTrace can be out of order with log statements
      */
-    public static void dTraceWhole(){
+    public static void dTraceWhole()
+    {
 
-        if (LOGGING_LEVEL_FILTER >= ERRORS_WARNINGS_INFO_DEBUG) {
+        if (LOGGING_LEVEL_FILTER >= ERRORS_WARNINGS_INFO_DEBUG)
+        {
             Throwable t = new Throwable();
             StackTraceElement[] elements = t.getStackTrace();
             StringBuilder builder = new StringBuilder();
-            for(StackTraceElement e : elements){
+            for (StackTraceElement e : elements)
+            {
                 builder.append("\n\t");
                 builder.append(e);
             }
-            Log.d(TAG_NO_FILTER, getTrace() + builder.toString());
+            Log.d(XLog.class.getName(), getTrace().trace + builder.toString());
         }
     }
 
-    private static String getTrace() {
+    private static Trace getTrace()
+    {
 
         int depth = 2;
         Throwable t = new Throwable();
@@ -122,6 +157,19 @@ public class XLog {
         int i = callerClassPath.lastIndexOf('.');
         String callerClassName = callerClassPath.substring(i + 1);
         String trace = callerClassName + ": " + callerMethodName + "() [" + lineNum + "] - ";
-        return trace;
+
+        return new Trace(trace, callerClassName);
+    }
+
+    public static class Trace
+    {
+        public String trace;
+        public String clazz;
+
+        public Trace(String trace, String clazz)
+        {
+            this.trace = trace;
+            this.clazz = clazz;
+        }
     }
 }
