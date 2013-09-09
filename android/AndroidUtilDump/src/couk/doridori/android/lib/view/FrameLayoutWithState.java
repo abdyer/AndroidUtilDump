9e77bb29e9957ee1340d7713bbef27d22e6f298b
Dorian Cussen <github@doridori.co.uk>
2013-09-09 11:04:49 +0100

    remove animation code and update javadoc - anim can just be set in xml via layoutTransitions

--- android/AndroidUtilDump/src/couk/doridori/android/lib/view/FrameLayoutWithState.java
+++ android/AndroidUtilDump/src/couk/doridori/android/lib/view/FrameLayoutWithState.java
@@ -27,6 +27,22 @@ import android.widget.TextView;
 import couk.doridori.android.lib.R;
 
 /**
+ * Copyright (C) 2011 Dorian Cussen
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ *
+ *
  * Like a normal frameLayout apart from this will swap out its views
  * depending on some data state.
  *
@@ -37,8 +53,6 @@ import couk.doridori.android.lib.R;
  * 'state_error_text'
  *
  * Animations can be setup but using layoutTransitions = true in the manifest (unless they have been globally disabled in the user settings)
- *
- * @author dorian.cussen
  */
 public class FrameLayoutWithState extends FrameLayout {
 
